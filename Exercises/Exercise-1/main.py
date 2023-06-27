ea2dd7cad2ef122a238fe2d5e5f73d3deabad780
onk <onksank16@gmail.com>
2023-06-27 13:31:47 +0530

    Revert "Completed first exercise"
    This reverts commit ce0acc1d3c083af1d0a14cf8b1b55e277d1dab76.

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,6 +1,4 @@
 import requests
-import os
-import shutil
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -15,32 +13,6 @@ download_uris = [
 
 def main():
     # your code here
-    directory = 'download'
-    parent_directory = 'C:/Users/Admin/sf/depract'
-    path = os.path.join(parent_directory,directory)
-
-    try:
-        os.mkdir(path)
-    except OSError as e:
-        print(e)
-
-    os.chdir(path)
-    print(path)
-    
-    for url in download_uris:
-        file_name = url.split('/')[-1]
-        r = requests.get(url,allow_redirects = True)
-        f = open(file_name,'wb')
-        print('file is  downloaded in download folder......')
-        f.write(r.content)
-        try:
-            shutil.unpack_archive(file_name,path)
-            f.close()
-            os.remove(file_name)
-            print(file_name + ' is deleted after extraction')
-        except OSError as e:
-            print(e)
-
     pass
 
 

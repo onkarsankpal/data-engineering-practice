fd58ab91bb4e9567d36ff6dac3649dd85bcf8c7b
Onkar Sankpal <1110onkarsankpal@gmail.com>
2023-06-27 13:51:28 +0530

    First exercise completed

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -13,6 +13,32 @@ download_uris = [
 
 def main():
     # your code here
+    directory = 'download'
+    parent_directory = 'C:/Users/Admin/sf/depract'
+    path = os.path.join(parent_directory,directory)
+
+    try:
+        os.mkdir(path)
+    except OSError as e:
+        print(e)
+
+    os.chdir(path)
+    print(path)
+    
+    for url in download_uris:
+        file_name = url.split('/')[-1]
+        r = requests.get(url,allow_redirects = True)
+        f = open(file_name,'wb')
+        print('file is  downloaded in download folder......')
+        f.write(r.content)
+        try:
+            shutil.unpack_archive(file_name,path)
+            f.close()
+            os.remove(file_name)
+            print(file_name + ' is deleted after extraction')
+        except OSError as e:
+            print(e)
+
     pass
 
 
